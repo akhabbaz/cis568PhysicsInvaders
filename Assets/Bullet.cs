11575a0a60b811dca66faf4e4d2232f22a177b1b
Anton Khabbaz <Anton.khabbaz@gmail.com>
2018-01-28 20:48:54 -0500

    Got the bullet prefab to work

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-        public Vector3 thrust;
-        public Quaternion heading;
+	public Vector3 thrust;
+	public Quaternion heading;
+	// store the camera so that one can 
+	// kill bullets off of the screen.
         // Use this for initialization
         void Start()
         {
@@ -22,5 +24,14 @@ public class Bullet : MonoBehaviour {
     // Update is called once per frame
     void Update()
     { //Physics engine handles movement, empty for now. }
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+	if (screenPos.y > Screen.height || screenPos.y < 0) {
+		Die();
+	}
     }
+    public void Die()
+    {
+	    Destroy(gameObject);
+    }
+
 }
